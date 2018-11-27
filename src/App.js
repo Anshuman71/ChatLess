e847a125b26f22a50f28d6d07d21bdfa53b9337a
Anshuman <anshu5074@gmail.com>
2018-11-27 18:13:04 +0530

    done sending

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIOClient from 'socket.io-client';
+
+const endpoint = 'http://localhost:2017';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.chatBox = React.createRef();
+    this.io = socketIOClient(endpoint);
+    this.state = {
+      message: '',
+      chatItems: [],
+    };
+  }
+  componentDidMount() {
+    this.io.on('newMessage', data => {
+      console.log({ data });
+      this.setState(prev => ({
+        chatItems: [...prev.chatItems, data.message.message],
+      }));
+    });
+  }
+  sendMessage = () => {
+    const { message } = this.state;
+    this.io.emit('sendMessage', { message });
+    this.setState({ message: '' });
+  };
+  changeMessage = event => {
+    this.setState({ message: event.target.value });
+  };
+  keyChange = event => {
+    if (event.key === 'Enter') this.sendMessage();
+  };
   render() {
+    const { chatItems } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div
+        style={{
+          display: 'flex',
+          height: '100vh',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          ref={this.chatBox}
+          style={{
+            height: 400,
+            width: 350,
+            border: '2px solid #ccc',
+            backgroundColor: '#f7f7f7',
+          }}
+        >
+          {chatItems.length
+            ? chatItems.map((item, i) => <p key={i}>{item}</p>)
+            : 'No chat'}
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', width: 350 }}>
+          <input
+            onChange={this.changeMessage}
+            value={this.state.message}
+            onKeyPress={this.keyChange}
+            style={{ flexGrow: 1 }}
+            type="text"
+          />
+          <button
+            onClick={this.sendMessage}
+            style={{
+              flexGrow: 1,
+              border: 'none',
+              fontSize: 18,
+              height: 30,
+              width: 80,
+            }}
           >
-            Learn React
-          </a>
-        </header>
+            Send
+          </button>
+        </div>
       </div>
     );
   }
